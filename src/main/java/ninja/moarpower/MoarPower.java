7ba05c57add0e3a49267ce4718bf584e478275dd
violentninjad <fiddlybilly@gmail.com>
2014-12-07 11:07:35 +0000

    First Steps :)

---
+++ src/main/java/ninja/moarpower/MoarPower.java
@@ -0,0 +1,15 @@
+package ninja.moarpower;
+
+import cpw.mods.fml.common.Mod;
+
+/** @author ViolentNinjaD
+    LGPLv3
+**/
+
+@Mod(modid = MoarPower.MOD_ID, name = MoarPower.MOD_NAME, version = MoarPower.MOD_VERSION)
+public class MoarPower 
+{
+    public static final String MOD_ID = "MoarPower";
+    public static final String MOD_NAME = "MOAR Power!";
+    public static final String MOD_VERSION = "@VERSION@";
+}
