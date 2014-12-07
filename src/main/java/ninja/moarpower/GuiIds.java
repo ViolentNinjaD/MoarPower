e1ad61013eafa8130c7ffa72d73c63317b89c579
violentninjad <fiddlybilly@gmail.com>
2014-12-07 10:54:14 +0000

    Starting fresh for 1.7.10

--- src/main/java/ninja/moarpower/GuiIds.java
+++
@@ -1,8 +0,0 @@
-package ninja.moarpower;
-/** @author ViolentNinjaD
-    LGPLv3
-**/
-public class GuiIds
-{
-    public static final int STONE_FANCIFIER = 0;
-}
--- src/main/java/ninja/moarpower/isteal/ee3/JsonItemStack.java
+++
@@ -1,9 +0,0 @@
-package ninja.moarpower.isteal.ee3;
-
-public class JsonItemStack
-{
-    public int stackSize;
-    public int itemID;
-    public byte[] compressedStackTagCompound;
-    public int itemDamage;
-}
